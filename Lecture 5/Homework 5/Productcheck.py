73ef74b511c075baf9783a4731f0b2aab4e2a73e
SiranushSevoyan <siranush.sevoyan.12@gmail.com>
2019-11-23 19:03:53 +0400

    Homework 5

--- Lecture 5/Homework 5/Productcheck.py
+++ Lecture 5/Homework 5/Productcheck.py
@@ -1,7 +1,6 @@
 products = {'candy':10,"juice": 5,"pen":50}
 
 def check(name, qtt):
-    #for key, value in products.items():
         if name in products and qtt<=products[name]:
             return True
         else:
